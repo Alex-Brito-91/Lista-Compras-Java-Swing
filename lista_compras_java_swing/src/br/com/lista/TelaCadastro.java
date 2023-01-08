bc415e21c157047a823d0409c946effd37ff723c
alex brito <alex.teixeira.brito@gmail.com>
2023-01-08 08:14:45 -0300

    Nova tela de cadastro

--- lista_compras_java_swing/src/br/com/lista/TelaCadastro.java
+++ lista_compras_java_swing/src/br/com/lista/TelaCadastro.java
@@ -5,46 +5,63 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JSpinner;
 import javax.swing.JButton;
+import javax.swing.JTable;
 
 public class TelaCadastro extends JFrame {
 	
 	private JTextField textProduto;
 	private JTextField textValor;
+	private JTable table;
+	TabelaModelo modelo = new TabelaModelo();
 	
 	public TelaCadastro() {
+		super("Cadastro de Produtos");
 		setResizable(false);
 		getContentPane().setLayout(null);
-		setSize(350,300);
+		setSize(600,500);
 		
-		JLabel lblProduto = new JLabel("Produto:");
-		lblProduto.setBounds(67, 35, 70, 15);
+		JLabel lblProduto = new JLabel("Produto");
+		lblProduto.setBounds(34, 12, 70, 15);
 		getContentPane().add(lblProduto);
 		
+		JLabel lblQuant = new JLabel("Quant");
+		lblQuant.setBounds(335, 11, 95, 17);
+		getContentPane().add(lblQuant);
+		
+		JLabel lblValorUn = new JLabel("Valor");
+		lblValorUn.setBounds(494, 12, 48, 15);
+		getContentPane().add(lblValorUn);
+		
 		textProduto = new JTextField();
-		textProduto.setBounds(139, 33, 142, 19);
+		textProduto.setBounds(34, 39, 188, 19);
 		textProduto.setColumns(10);
 		getContentPane().add(textProduto);
 		
-		JLabel lblQuant = new JLabel("Quantidade:");
-		lblQuant.setBounds(42, 64, 95, 17);
-		getContentPane().add(lblQuant);
-		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(139, 63, 54, 20);
-		getContentPane().add(spinner);
-		
-		JLabel lblValorUn = new JLabel("Valor:");
-		lblValorUn.setBounds(89, 93, 48, 15);
-		getContentPane().add(lblValorUn);
-		
 		textValor = new JTextField();
 		textValor.setColumns(10);
-		textValor.setBounds(139, 91, 54, 19);
+		textValor.setBounds(494, 39, 70, 19);
 		getContentPane().add(textValor);
 		
-		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.setBounds(115, 171, 117, 25);
-		getContentPane().add(btnCadastrar);
+		JSpinner spinnerQuant = new JSpinner();
+		spinnerQuant.setBounds(335, 39, 54, 20);
+		getContentPane().add(spinnerQuant);
+		
+		table = new JTable();
+		table.setModel(modelo);
+		table.setBounds(34, 90, 530, 280);
+		getContentPane().add(table);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.setBounds(61, 397, 117, 25);
+		getContentPane().add(btnSalvar);
+		
+		JButton btnEditar = new JButton("Editar");
+		btnEditar.setBounds(239, 397, 117, 25);
+		getContentPane().add(btnEditar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBounds(417, 397, 117, 25);
+		getContentPane().add(btnExcluir);
 		
 	}
 	
